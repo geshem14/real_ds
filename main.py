54a4bc4d012ff8f0d543ce0fd85e52c8c0acf2c6
geshem14 <geshem1414@gmail.com>
2020-02-18 21:03:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ def game_core_v3(number):
        Функция принимает загаданное число и возвращает число попыток'''
     count = 0
     low=1
-    up=99
+    up=101
     predict = np.random.randint(1,100)
-    while number != predict:
-        count+=1
-        if number > predict:
-            up=number
-            number=(up-low)//2+low
-        elif number < predict: 
-            low=number
+    if number==predict:
+        count=1
+    else:
+        while number != predict:
+            count+=1
+            if number > predict:
+                up=number
+            elif number < predict: 
+                low=number
             number=(up-low)//2+low
     return(count) 
 def score_game(game_core_v3):
     '''Запускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число'''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1, 101, size=(30))
+    random_array = np.random.randint(1, 101, size=(1000))
     for number in random_array:
         count_ls.append(game_core_v3(number))
     score = int(np.mean(count_ls))
